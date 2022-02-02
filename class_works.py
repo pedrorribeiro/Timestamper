410172fe4749057d87b85d2315c912ca3c2fbc79
pedro <p.ribeiro.engenheiro@gmail.com>
2022-02-02 15:44:57 +0000

    added loader and settings.json file

--- class_works.py
+++ class_works.py
@@ -1,7 +1,27 @@
 import time
 import asyncio
+import json
 from dataclasses import dataclass
 
+
+@dataclass
+class Loader:
+    settings: dict
+    phase_list: list
+    n_phases: int
+    n_rounds: list
+    round_timers: list
+
+    def __innit__(self):
+        self.settings = json.load('settings.json')
+
+    def split_settings(self):
+        self.phase_list = self.settings['phase_list']
+        self.n_phases = self.settings['#phases']
+        for i in self.n_phases:
+            self.n_rounds.append(self.settings['#rounds'][i])
+        for i in self.n_phases:
+            self.round_timers.append(self.settings['round_timers'][i])
 
 @dataclass
 class TimeManagement:
